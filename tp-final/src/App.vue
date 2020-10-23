9614009897389666a6d8fffc7371303f32c53b0c
Sebastian <sebastianfelice27@gmail.com>
2020-10-23 11:49:50 -0300

    Prueba de subida de componentes

--- tp-final/src/App.vue
+++ tp-final/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Que onda monos"/>
+   <!--  <Peliculas msg ="Hola"> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+ /* import Peliculas from '.components/Peliculas.vue'  */
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+   /*  Peliculas */
   }
 }
 </script>
---
+++ tp-final/src/components/Peliculas.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="peliculas">
+    <ul>
+        <li v-for="(pelicula,index) in peliculas" :key="index">
+            <peliculas-item :title="pelicula.nombre"> </peliculas-item>
+
+        </li>
+    </ul>
+    
+  </div>
+</template>
+
+
+<script>
+    
+
+    Vue.component ('peliculas-item',
+    {
+        template:`
+        <div>
+        <h1> {{titulo}} </h1>
+        <h2> alo </h2>
+        
+        </div>
+        
+        `,
+
+         
+           
+                
+            props:['titulo']
+        
+        
+        
+        
+    })
+
+    
+
+    new Vue (
+{
+    el:'#peliculas',
+    data()
+    {
+        return{
+            peliculas:[{nombre:'Avatar'},{nombre:'Narnia'}]
+        }
+    }
+}
+
+    )
+
+  export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+    
+    
+</script>
+    
