ba62d5d351ec86bdc900d16d64e47a6c8ca70e94
Martin Jamschon <mjamschon@gmail.com>
2020-10-23 20:55:15 -0300

    productos1

--- tp-final/src/App.vue
+++ tp-final/src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Pepe"/>
-   <Peliculas />
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Pepe"/>
+      <Peliculas />
+      <Productos />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue' 
-import Peliculas from './components/Peliculas.vue'  
+import Peliculas  from './components/Peliculas.vue'
+import Productos  from './components/Productos.vue'  
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Peliculas 
+    Peliculas,
+    Productos 
   }
 }
 </script>
