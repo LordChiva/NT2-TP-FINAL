234adae2548985813e6728b0dd123144947a8c72
Alan <alubron99@gmail.com>
2020-10-23 17:03:59 -0300

    primer subida

--- tp-final/src/App.vue
+++ tp-final/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="hoal"/>
+    <HelloWorld msg="Pepe"/>
    <!--  <Peliculas msg ="Hola"> -->
   </div>
 </template>
