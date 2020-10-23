d7a9059d692d94df728f55c7566956a4c7f8928b
Martin Jamschon <mjamschon@gmail.com>
2020-10-23 16:49:32 -0300

    prueba

--- tp-final/src/App.vue
+++ tp-final/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Que onda monos"/>
+    <HelloWorld msg="hoal"/>
    <!--  <Peliculas msg ="Hola"> -->
   </div>
 </template>
